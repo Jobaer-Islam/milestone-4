fe87e09543cf826e27167ee00de4058cffc2df50
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-08 11:24:55 +0600

    comparison

---
+++ js-conditionals/comparison.js
@@ -0,0 +1,12 @@
+// console.log(5 > 10);   // false
+// console.log(5 < 10);   // true
+// console.log(5 == 10);  // false
+// console.log(10 == 10); // true
+// console.log(15 >= 10); // true
+// console.log(5 >= 10);  // false
+// console.log(5 <= 10);  // true
+// console.log(10 <= 10); // true
+// console.log(100 <= 10); // false
+
+console.log(10 == "10"); // true
+console.log(10 === "10"); // false
