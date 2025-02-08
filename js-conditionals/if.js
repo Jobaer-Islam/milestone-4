89082c9ea217fbc8356436a66f5106c484a5c285
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-08 12:17:46 +0600

    if

---
+++ js-conditionals/if.js
@@ -0,0 +1,23 @@
+/* 
+if(condition){
+    execute code if condition is true    
+}
+else{
+    execute code if condition is false
+}
+
+
+
+*/
+
+console.log("hello world!");
+if (13 > 10) {
+    console.log('I have a smaller value');
+
+}
+
+var birianyPrice = 200;
+if (birianyPrice < 300) {
+    console.log('mama give me some kacchi');
+    
+}
