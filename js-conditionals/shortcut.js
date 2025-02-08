9619505ae8f20a180f8b87bbd038692bba1d1054
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-08 15:47:41 +0600

    some shortcut

---
+++ js-conditionals/shortcut.js
@@ -0,0 +1,19 @@
+const isLeader = true;
+// if(isLeader === true)
+if(isLeader){
+    console.log('leader asche! chair charo');
+}
+else{
+    console.log('vitore dukte dibi na');
+    
+}
+
+const isPassed = false;
+// if(isPassed === false)
+if( !isPassed){
+    console.log('toke biye diya dimu');
+    
+}else{
+    console.log('tumi amader noyoner moni');
+    
+}
