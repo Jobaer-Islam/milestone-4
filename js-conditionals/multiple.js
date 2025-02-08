59023181e479ca1c73e23947a8faf6912dd828f9
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-08 12:32:32 +0600

    if else

---
+++ js-conditionals/multiple.js
@@ -0,0 +1,43 @@
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+
+// if(salary > 20000 && height > 66){
+//     console.log('Su....patro!');
+    
+// }
+// else{
+//     console.log('onno patro khuji');
+    
+// }
+
+
+
+// if(salary > 25000 || height > 72){
+//     console.log('eso baba kobul');
+// }
+// else{
+//     console.log('vaag tui mokbul?');
+    
+// }
+
+
+
+// more and more conditions
+
+// if(salary > 25000 || height > 72 || isBCS == true){
+//     console.log('eso baba kobul');
+// }
+// else{
+//     console.log('vaag tui mokbul?');
+    
+// }
+
+
+if(salary > 25000 && height > 72 && isBCS == true){
+    console.log('eso baba kobul');
+}
+else{
+    console.log('vaag tui mokbul?');
+    
+}
