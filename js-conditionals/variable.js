60cd1ff724f4644d268eac5102d55b728fe9b395
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-08 10:44:54 +0600

    start javascript

---
+++ js-conditionals/variable.js
@@ -0,0 +1,23 @@
+// Variable ==> container
+
+// number
+var sunglassPricer = 80;
+
+// string
+var subject = "CSE"
+
+// boolean
+var passed = true;
+
+/*
+Naming conventions
+1.Single word
+2. no quote
+3. no gap or dash
+4. can not start with number
+5. can not be a keyword
+6. prefer camelCase 
+
+*/
+
+var lastExamResult = 88;
