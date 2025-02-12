9f77ca6e8de4596e9253a95c7a8045be7db41a9b
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-13 00:28:52 +0600

    INCLUES

---
+++ ConceptsOfArray/includes.js
@@ -0,0 +1,10 @@
+const friends = ['vai', 'bros', 'sis', 'bon'];
+
+console.log(friends.includes('vai'));
+console.log(friends.includes('nai'));
+
+if(friends.includes('Khailam')){
+    console.log("kire khas nai?");
+}else{
+    console.log("khamu na re vai?");
+}
