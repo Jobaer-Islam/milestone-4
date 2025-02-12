c1f3b3f4a96cb55e86288b72a0a8fad94ab25309
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-12 23:36:19 +0600

    array js

---
+++ ConceptsOfArray/array.js
@@ -0,0 +1,15 @@
+const numbers = [12, 22, 54, 65, 99];
+console.log(numbers.length);
+console.log(numbers);
+console.log(numbers[4]);
+
+// get element value by index
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or update element value by index
+numbers[4] = 100;
+console.log(numbers);
+
+
+
