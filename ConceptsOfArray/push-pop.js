bb1aff76cb0e6c3dca56e8323c44bca1f9404995
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-12 23:53:51 +0600

    push-pop

---
+++ ConceptsOfArray/push-pop.js
@@ -0,0 +1,26 @@
+const ages = [];
+const numbers = [12, 22, 54, 65, 99];
+// console.log(numbers);
+numbers.push(44);
+numbers.push(27);
+numbers.push(27, 33, 51);
+console.log(numbers);
+
+const friends = ['vai', 'bros', 'sis', 'bon'];
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+const out1 = friends.pop();
+
+friends.pop();
+console.log(friends);
+const out2 = friends.pop();
+
+console.log(out1, out2);
+
+
+
+
+
+
