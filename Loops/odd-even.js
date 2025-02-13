4545d1329835ecbedbae61b98290d11d8f81a0c0
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-13 23:35:26 +0600

    even-odd

---
+++ Loops/odd-even.js
@@ -0,0 +1,16 @@
+//  even
+for(let i = 0; i <= 20; i++) {
+    if(i%2 == 0 ){
+        console.log('even: '+i);
+
+    }    
+}
+
+
+// odd
+for(let i = 0; i <= 20; i++) {
+    if(i%2 !== 0 ){
+        console.log('odd: '+ i);
+
+    }    
+}
