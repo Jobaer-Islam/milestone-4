30687c560117f1a7851356c71887c7a8336635ed
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-13 23:03:16 +0600

    loops

---
+++ ConceptsOfArray/indexOf.js
@@ -0,0 +1,4 @@
+const friends = ['vai', 'bros', 'sis', 'bon'];
+
+console.log(friends.indexOf('bros'))
+console.log(friends.indexOf('ami'))
---
+++ Loops/for-loop.js
@@ -0,0 +1,6 @@
+const numbers = [12, 98, 101, 10, 78 ];
+console.log(numbers);
+
+for (const num of numbers) {
+    console.log(num);
+} 
