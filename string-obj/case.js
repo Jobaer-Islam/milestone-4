e2e319ad904bad3316374bd004e15497b7030f34
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-14 11:53:32 +0600

    string

---
+++ string-obj/case.js
@@ -0,0 +1,30 @@
+const school = 'Rajuk Uttara Model School';
+
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+
+    console.log(' I am reading the book aibar exm fatay dibo');
+    
+}else{
+    console.log('hudai prista ultai vat khawar jnne');
+
+}
+
+
+ const drink = 'water';
+ const liquid = ' water  ';
+
+ if ( drink.trim() === liquid.trim()){
+    console.log('panir opr nam jibn');
+    
+ }else{
+    console.log('somodre pani onk kintu khawa jay na');
+    
+ }
---
+++ string-obj/string.js
@@ -0,0 +1,23 @@
+const country = 'Bangladesh';
+const division = `Chattagram`;
+const district = "Cumilla"
+
+const thana = new String('Burichang') //object
+
+const numbers = [54, 55, 56, 57, 58, 59, 60, 61]
+console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[1]);
+numbers[1] = 11;
+console.log(numbers);
+
+
+const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[3]);
+console.log(capital);
+capital[0] = 'F'; // string is immutable
+console.log(capital);
+
+
+
