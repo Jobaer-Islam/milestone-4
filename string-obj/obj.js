8769f3e920f45f203d1b548c1ee3f9063b19226e
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-14 20:14:47 +0600

    declare obj

---
+++ string-obj/obj.js
@@ -0,0 +1,35 @@
+ // Define constants for primitive types
+const age = 21;
+const school = 'rifle square';
+const isPassed = true;
+
+// Define an array of subjects
+const subjects = ['bangla', 'English', 'Physics', 'Math'];
+
+// Define constant properties for a bottle
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+};
+
+// Define constant properties for a subject
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 Feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+};
+
+// Output to console
+console.log("Age:", age);
+console.log("School:", school);
+console.log("Passed:", isPassed);
+console.log("Subjects:", subjects);
+console.log("Bottle:", bottle);
+console.log("Subject Detail:", subject);
