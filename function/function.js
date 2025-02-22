df0802ca13253957b273e9fca160b9c720085aa5
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-22 18:22:37 +0600

    learning functions

---
+++ function/function.js
@@ -0,0 +1,19 @@
+const number = 45;
+// function declaration
+function fanOffKOr(){
+    console.log("bosa thke oth")
+    console.log("walk towards the switch")
+    console.log("click the switch button")
+}
+
+function brushTeeth(){
+    console.log("take the brush");
+    console.log("add paste");
+    console.log("2min goso and koli koro");
+
+}
+
+// call the function
+
+fanOffKOr();
+brushTeeth();
---
+++ function/parameter.js
@@ -0,0 +1,14 @@
+function square(number){
+    console.log("value of the number parameter ", number);
+    const borgo = number * number;
+    console.log("square of the given number", borgo);
+}
+square(4);
+
+console.log("-----------------");
+
+square(5);
+
+console.log("----------------");
+
+square(6);
